c32394bd9ea80098dfd5be94e1656dd7b858069d
Vladimir <cbb110@rambler.ru>
2024-09-15 13:01:30 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/ChutHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization; // для атрибута Authorize
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string to)
+        {
+            // получение текущего пользователя, который отправил сообщение
+            //var userName = Context.UserIdentifier;
+            if (Context.UserIdentifier is string userName)
+            {
+                await Clients.Users(to, userName).SendAsync("Receive", message, userName);
+            }
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"Приветствуем {Context.UserIdentifier}");
+            await base.OnConnectedAsync();
+        }
+    }
+}
---
+++ WebApplication2/Program.cs
@@ -0,0 +1,15 @@
+
+using WebApplication2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();      // подключема сервисы SignalR
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+
+app.Run();
